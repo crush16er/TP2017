7ae206f0b4792bd8a0cd1f3c04f966f44e55711d
Sebastian <sebastianwawrzyniak1@wp.pl>
2017-10-15 09:09:50 +0200

    chng

--- xxxx/src/xxxx/HelloWord.java
+++ xxxx/src/xxxx/HelloWord.java
@@ -3,7 +3,7 @@ package xxxx;
 public class HelloWord {
 
 	public static void main(String[] args) {
-		System.out.println("1");
+		System.out.println("0");
 		System.out.println("2");
 		System.out.println("3");
 
