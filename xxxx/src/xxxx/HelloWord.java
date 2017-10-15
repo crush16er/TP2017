ee93a01645dfe3179f025a83e8e0cf6a80030188
Sebastian <sebastianwawrzyniak1@wp.pl>
2017-10-15 08:46:38 +0200

    changing
    data

--- xxxx/src/xxxx/HelloWord.java
+++ xxxx/src/xxxx/HelloWord.java
@@ -3,9 +3,9 @@ package xxxx;
 public class HelloWord {
 
 	public static void main(String[] args) {
-		System.out.println("0");
+		System.out.println("1");
 		System.out.println("2");
-		System.out.println("2");
+		System.out.println("3");
 
 	}
 
